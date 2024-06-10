eb9930a8afd7f26c04835654893e589d981cbe4c
Baudson Dylan <dylan.baudson1@etu.univ-lorraine.fr>
2024-06-10 11:57:49 +0200

    librairie :(

--- services/Converter.java
+++ services/Converter.java
@@ -5,30 +5,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.json.JSONArray;
+import org.json.*;
 
 public class Converter implements ServiceConverter {
     private String password;
     private String username;
 
-    private final String url="jdbc:oracle:thin:@charlemagne.iutnc.univ-lorraine.fr:1521:infodb";
+    private final String url = "jdbc:oracle:thin:@charlemagne.iutnc.univ-lorraine.fr:1521:infodb";
 
-    Converter(String u, String p){
-        this.password=p;
-        this.username=u;
+    Converter(String u, String p) {
+        this.password = p;
+        this.username = u;
     }
-    
+
     @Override
-    public JSON getAllRestaurantData() throws RemoteException, ClassNotFoundException, SQLException {
+    public JSONArray getAllRestaurantData() throws RemoteException, ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
 
-        Connection connection= DriverManager.getConnection(url, username, password);
+        Connection connection = DriverManager.getConnection(url, username, password);
 
-        Statement st=connection.createStatement();
+        Statement st = connection.createStatement();
 
-        ResultSet rs=st.executeQuery("SELECT * FROM RESTAURANT");
+        ResultSet rs = st.executeQuery("SELECT * FROM RESTAURANT");
 
-        while(rs.next()){
+        while (rs.next()) {
 
         }
 
--- services/ServiceConverter.java
+++ services/ServiceConverter.java
@@ -1,7 +1,9 @@
+import org.json.JSONArray;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.sql.SQLException;
 
 public interface ServiceConverter extends Remote{
-    public String getAllRestaurantData() throws RemoteException, ClassNotFoundException, SQLException;
+    public JSONArray getAllRestaurantData() throws RemoteException, ClassNotFoundException, SQLException;
 }
