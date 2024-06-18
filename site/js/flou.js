48b0e63cb95fe47db60d6f05f6beec3a5786aaed
axeldg54 <dung1u@etu.univ-lorraine.fr>
2024-06-18 08:37:15 +0200

    web

--- site/js/flou.js
+++ site/js/flou.js
@@ -1,7 +1,10 @@
 export function show() {
+    let form = document.getElementById('form');
+    form.classList.remove('formOut');
+    form.classList.add('formIn');
+    form.style.bottom = '25%';
     let flou = document.getElementById('flou');
     flou.style.display = 'block';
-    document.getElementById('form').style.left = "25%";
     flou.addEventListener('click', () => {
         hide();
     });
@@ -9,9 +12,10 @@ export function show() {
 
 export function hide() {
     let form = document.getElementById('form');
-    form.style.left = "-100%";
+    form.classList.remove('formIn');
+    form.classList.add('formOut');
+    form.style.bottom = '-100%';
     let flou = document.getElementById('flou');
     flou.style.display = 'none';
     flou.removeEventListener('click', hide);
-
 }
